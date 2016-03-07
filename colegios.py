27062748d663f353cc9d9132b07e925413f5497e
Alejandro Palomino García <muchoh123@hotmail.com>
2016-03-07 12:39:00 +0100

    Ejercicio extra terminado

--- colegios.py
+++ colegios.py
@@ -7,11 +7,15 @@ print "B= CUENTA EL NUMERO DE COLEGIOS QUE CONTIENE EL FICHERO"
 print "C= DEVOLVER URL DEL COLEGIO SEGÚN UN NÚMERO DE TELÉFONO"
 print "D= DEVOLVER NOMBRE DEL COLEGIO SEGÚN SU DIRECCION"
 print "E= IMPRIME EL HORARIO DEL COLEGIO SEGÚN SU COORDENADA"
+print "F= PIDE POR TECLADO UNA SUBCADENA Y SI LA DESCRIPCIÓN DEL COLEGIO LO CONTIENE, LO INTRODUCE EN UN FICHERO HTML"
 print "EXIT= PARA SALIR"
 
 print "------------------------------------------"
 from lxml import etree
 import lxml.html, lxml.etree
+import sys  
+reload(sys)  
+sys.setdefaultencoding('utf8')
 
 doc=etree.parse('colegios.xml')
 raiz=doc.getroot()
@@ -57,12 +61,32 @@ while tecla!="EXIT":
 			if buscador==c:
 				print h.text
 			
+	if tecla=="F":
+		buscador=raw_input("Dime algún dato de la descripción del colegio: ")
+		import os
+		colegios=etree.parse('colegios.xml')
+		raiz=colegios.getroot()
+		descripcion=doc.xpath("/directorios/directorio/descripcion/text()")
+		nombre=doc.xpath("/directorios/directorio/nombre/text()")
+		imagen=doc.xpath("/directorios/directorio/foto/text()")
+		f=open("html.txt","w")
+		for d,n,i in zip(descripcion,nombre,imagen):
+			if d.find(buscador)>=0:
+				a="<h1>"+n+"</h1>"
+				b="<p>"+d+"</p>"
+				c="<img src="+i+"/>"
+				f.write(a+"\n")
+				f.write(b+"\n")
+				f.write(c+"\n")
+				f.write("\n")
+		f.close()
 	print "------------------------------------------"
 	print "A= LISTA EL NOMBRE DE TODOS LOS COLEGIOS "
 	print "B= CUENTA EL NUMERO DE COLEGIOS QUE CONTIENE EL FICHERO"
 	print "C= DEVOLVER URL DEL COLEGIO SEGÚN UN NÚMERO DE TELÉFONO"
 	print "D= DEVOLVER NOMBRE DEL COLEGIO SEGÚN SU DIRECCION"
 	print "E= IMPRIME EL HORARIO DEL COLEGIO SEGÚN SU COORDENADA"
+	print "F= PIDE POR TECLADO UNA SUBCADENA Y SI LA DESCRIPCIÓN DEL COLEGIO LO CONTIENE, LO INTRODUCE EN UN FICHERO HTML"
 	print "EXIT= PARA SALIR"
 
 	print "------------------------------------------"
